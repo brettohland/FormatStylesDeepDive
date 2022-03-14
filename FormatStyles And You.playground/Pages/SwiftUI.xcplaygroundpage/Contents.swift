d90405315b98a18d24956b345388b7dea3fdbc02
brett ohland <brett@ohland.ca>
2022-03-13 22:22:55 -0600

    Fixing SwiftUI section

--- FormatStyles And You.playground/Pages/SwiftUI.xcplaygroundpage/Contents.swift
+++ FormatStyles And You.playground/Pages/SwiftUI.xcplaygroundpage/Contents.swift
@@ -1,34 +1,31 @@
 //: [Previous](@previous)
 
-import SwiftUI
 import PlaygroundSupport
+import SwiftUI
 
 struct ContentView: View {
-    struct ContentView: View {
-        static let twosdayDateComponents = DateComponents(
-            year: 2022,
-            month: 2,
-            day: 22,
-            hour: 2,
-            minute: 22,
-            second: 22,
-            nanosecond: 22
-        )
+    static let twosdayDateComponents = DateComponents(
+        year: 2022,
+        month: 2,
+        day: 22,
+        hour: 2,
+        minute: 22,
+        second: 22,
+        nanosecond: 22
+    )
 
-        var twosday: Date {
-            Calendar(identifier: .gregorian).date(from: ContentView.twosdayDateComponents)!
-        }
-
-        var body: some View {
-            VStack {
-                Text(twosday, format: Date.FormatStyle(date: .complete, time: .complete))
-                Text(twosday, format: .dateTime.hour())
-                Text(twosday, format: .dateTime.year().month().day())
-            }
-            .padding()
-        }
+    var twosday: Date {
+        Calendar(identifier: .gregorian).date(from: ContentView.twosdayDateComponents)!
     }
 
+    var body: some View {
+        VStack {
+            Text(twosday, format: Date.FormatStyle(date: .complete, time: .complete))
+            Text(twosday, format: .dateTime.hour())
+            Text(twosday, format: .dateTime.year().month().day())
+        }
+        .padding()
+    }
 }
 
-PlaygroundPage.current.setLiveView(ContentView())//: [Next](@next)
+PlaygroundPage.current.setLiveView(ContentView()) //: [Next](@next)
